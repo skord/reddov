c125c2360c3d8e73db8c057dd5210592024590ec
Mike Danko <danko@theorypump.com>
2011-03-31 15:25:09 -0400

    cache fiddle

--- reddov.rb
+++ reddov.rb
@@ -13,23 +13,38 @@ configure :production do
 end
 
 get '/' do
-  # response['Cache-Control'] = 'public, max-age=5'
-  # cache 'cache1', :expiry => 5 do
+  response['Cache-Control'] = 'public, max-age=20'
+  #
+  # Caching for something rediculously small. 
+  # It should give the appearance of hitting refresh
+  # and getting a new page, but the folks will be
+  # be hitting the cache instead. 
+  #
+  # Three seconds about does it. It's the difference
+  # between ~200 requests per second and 6
+  
+  cache 'cache1', :expiry => 2 do
     @markoved_headlines = markoved_headlines
     erubis :index
-  # end
+  end
 end
 
 
 def reddit_json
+  @reddit_json ||= fetch_and_cache_reddit_json 
+end
+
+def fetch_and_cache_reddit_json
   # Cache the reddit stuff. We don't need it that often for fun.
   reddit_json_uri = 'http://www.reddit.com/.json?count=100'
   cached_reddit_json = settings.cache.get('reddit_json')
   if cached_reddit_json.nil? 
     parsed_json = JSON.parse(open(reddit_json_uri).read)
     settings.cache.set('reddit_json', parsed_json, ttl = 300)
+    puts "set"
     return parsed_json
   else
+    puts 'get'
     return cached_reddit_json
   end
 end
